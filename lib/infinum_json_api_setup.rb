c7e1e1b821983434166c5a6597a0f96bb6d7b3a9
Nika Jukic <nika.jukic@infinum.hr>
2020-10-28 15:00:35 +0100

    Fix requiring gem files

--- lib/infinum_json_api_setup.rb
+++ lib/infinum_json_api_setup.rb
@@ -4,7 +4,8 @@ require 'responders'
 
 require 'infinum_json_api_setup/error'
 require 'infinum_json_api_setup/json_api/error_serializer'
+require 'infinum_json_api_setup/json_api/parameter_parser'
 require 'infinum_json_api_setup/json_api/serializer_options'
 require 'infinum_json_api_setup/json_api/responder'
 
-require 'infinum_json_api_setup/json_api/rails'
+require 'infinum_json_api_setup/rails'
