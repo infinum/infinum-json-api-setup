f3dac09bbb7f1db9154cf76d2e0cbe2df5f2ce7a
Vedran Hrncic <vedran.hrncic@infinum.hr>
2021-07-11 19:44:49 +0200

    register location sort params

--- spec/dummy/app/web/api/v1/locations/query.rb
+++ spec/dummy/app/web/api/v1/locations/query.rb
@@ -2,6 +2,10 @@ module Api
   module V1
     module Locations
       class Query < Jsonapi::QueryBuilder::BaseQuery
+        default_sort created_at: :asc
+
+        sorts_by :latitude
+        sorts_by :longitude
       end
     end
   end
