81e70c1b8eb5cc24842387cfe42d7f6c0b6d6496
Vedran Hrncic <vedran.hrncic@infinum.hr>
2021-07-11 19:40:34 +0200

    add show action to location controller

--- spec/dummy/app/controllers/api/v1/locations_controller.rb
+++ spec/dummy/app/controllers/api/v1/locations_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class LocationsController < BaseController
+      include Pundit
+
       # GET /api/v1/locations
       def index
         q = Api::V1::Locations::Query.new(Location.all, params.to_unsafe_hash)
@@ -15,8 +17,25 @@ module Api
         respond_wih location
       end
 
+      # GET /api/v1/locations/:id
+      def show
+        location = authorize(Location.find(params[:id]))
+
+        respond_with location
+      end
+
+      private
+
       def permitted_params
         params.require(:location).permit(:latitude, :longitude)
+      end
+
+      def authorize(record)
+        super(record, policy_class: Api::V1::Locations::Policy)
+      end
+
+      def pundit_user
+        nil
       end
     end
   end
