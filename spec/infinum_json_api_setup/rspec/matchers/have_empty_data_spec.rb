17951f801c6b82501b9037a8914f49ae25efb9ae
Vedran Hrncic <vedran.hrncic@infinum.hr>
2021-09-20 21:50:15 +0200

    add specs for have_empty_data matcher

---
+++ spec/infinum_json_api_setup/rspec/matchers/have_empty_data_spec.rb
@@ -0,0 +1,43 @@
+describe InfinumJsonApiSetup::RSpec::Matchers::HaveEmptyData do
+  include TestHelpers::Matchers::Response
+
+  describe 'usage' do
+    context 'when data is empty' do
+      it 'matches' do
+        response = response_with_body(JSON.dump(data: {}))
+
+        expect(response).to have_empty_data
+      end
+    end
+
+    context "when data isn't empty" do
+      it 'fails and describes failure reason' do
+        response = response_with_body(JSON.dump(data: { a: 1 }))
+
+        expect do
+          expect(response).to have_empty_data
+        end.to fail_with("Expected response data({\"a\"=>1}) to be empty, but isn't")
+      end
+    end
+
+    context "when response isn't valid JSON" do
+      it 'fails and describes failure reason' do
+        response = response_with_body('')
+
+        expect do
+          expect(response).to have_empty_data
+        end.to fail_with('Failed to parse response body')
+      end
+    end
+
+    context "when response doesn't contain data attribute" do
+      it 'fails and describes failure reason' do
+        response = response_with_body('{}')
+
+        expect do
+          expect(response).to have_empty_data
+        end.to fail_with('Failed to extract data from response body')
+      end
+    end
+  end
+end
